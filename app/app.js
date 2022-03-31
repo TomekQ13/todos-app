20b4d667956a95f59505ceb413f30e56d20feb81
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-03-31 10:50:33 +0200

    added functionality to buttons

--- app/app.js
+++ app/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const { v4: uuidv4 } = require('uuid')
+const methodOverride = require('method-override')
+
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
 const { Client } = require('pg')
 const client = new Client({
@@ -23,9 +28,52 @@ app.get('/', async (req, res) => {
     } catch (e) {
         console.error(e)
     }
+    const todosToDo = resp.rows.filter(todo => todo.done === false)
+    const todosDone = resp.rows.filter(todo => todo.done === true)
     
-    return res.render('index', { todos: resp.rows })
+    return res.render('index', { todosToDo: todosToDo, todosDone: todosDone })
 })
+
+app.post('/', async (req, res) => {
+    let respone
+    try {
+        respone = await client.query(`
+            insert into todos (id, text)
+            values ($1, $2)
+            `, [uuidv4(), req.body.todo])
+    } catch (e) {
+        console.error(e)
+    }
+    return res.redirect('/')
+})
+
+app.put('/:todoid', async (req, res) => {
+    let resp
+    try {
+        resp = await client.query(`
+            update todos
+            set done = true
+            where id = $1
+        `, [req.params.todoid])
+    } catch (e) {
+        console.error(e)
+    }
+    return res.redirect ('/')
+})
+
+app.delete('/:todoid', async (req, res) => {
+    let resp
+    try {
+        resp = await client.query(`
+            delete from todos
+            where id = $1
+        `, [req.params.todoid])
+    } catch (e) {
+        console.error(e)
+    }
+    return res.redirect ('/')
+})
+
 
 app.listen(3000, () => {
     console.log('Application started and listening on port 3000')
