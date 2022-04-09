1cc3fa819bafd582a60f264f563a1d1d69453c93
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-04-09 19:21:26 +0200

    changed the env var name

--- app/app.js
+++ app/app.js
@@ -12,7 +12,7 @@ const { Client } = require('pg')
 const client = new Client({
     user: process.env.POSTGRES_RW_USER,
     host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_RW_PASSWORD,
     port: process.env.POSTGRES_PORT
 })
