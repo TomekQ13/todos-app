9aef94fa5014868566662fcb32229230d7cf750e
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-04-09 18:32:52 +0200

    added user creation

--- scripts/ddl.sql
+++ scripts/ddl.sql
@@ -15,3 +15,6 @@ values ('2', 'Write CSS');
 
 insert into todos (id, text)
 values ('3', 'Our unique todo');
+
+createuser app_rw;
+grant select, insert, update, delete on todos in schema public to app_rw;
